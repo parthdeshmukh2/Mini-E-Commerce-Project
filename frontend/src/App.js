de3821cffc00f678254c4284477645b17fc42fab
parthdeshmukh2 <parthdeshmukh99@gmail.com>
2022-11-17 13:39:33 +0530

    initail setup completed

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MainRoutes from './Pages/MainRoutes';
+
+function App() {
+  return (
+    <div >
+    <MainRoutes/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Redux/AppReducer/reducer.js
@@ -0,0 +1,27 @@
+import * as types from "./actionTypes";
+
+const initialState = {
+    data:[],
+    isLoading:false,
+    isError:false
+};
+
+export const reducer = (state=initialState, action)=>{
+
+    const {type, payload} = action;
+
+    switch(type){
+
+      case types.GET_DATA_REQUEST:
+        return {...state, isLoading:true, isError:false};
+
+        case types.GET_DATA_SUCCESS:
+            return {...state, isLoading:false, isError:false, data:payload}
+
+            case types.GET_DATA_FAILURE:
+                return {...state, isLoading:false, isError:false}
+
+        default : return state;
+    }
+
+}
